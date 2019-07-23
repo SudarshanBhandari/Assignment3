2fa8c88de765795ef76e74d7a692122f0230d432
SudarshanBhandari <47011252+SudarshanBhandari@users.noreply.github.com>
2019-07-23 18:54:35 -0400

    Updated change for image

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -5,27 +5,27 @@ export const SONG : songInfo[]=[{
     artist:'Martin Grrrix & Dua Lipa',
     genre:'Future bass',
     yearReleased:2017,
-    pic:'../assets/img/a.jpg'
+    pic:'./assets/img/a.jpg'
 },
 {
     songName:'Rockstar',
     artist:'Nickelback',
     genre:'Hard Rock',
     yearReleased:2005,
-    pic:'../assets/img/b.jpg'
+    pic:'./assets/img/b.jpg'
 },
 {
     songName:'Nothing Else Matters',
     artist:'Metallica',
     genre:'Rock',
     yearReleased:1991,
-    pic:'../assets/img/c.jpg'
+    pic:'./assets/img/c.jpg'
 },
 {
     songName:'Bring Me To Life',
     artist:'Evanescence',
     genre:'Nu Metal',
     yearReleased:2003,
-    pic:'../assets/img/d.jpeg'
-},
+    pic:'./assets/img/d.jpeg'
+}
 ]
