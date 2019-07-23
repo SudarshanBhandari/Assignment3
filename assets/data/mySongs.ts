8852ef5423b54ffd72bf98ae928fa1ea46a91083
SudarshanBhandari <47011252+SudarshanBhandari@users.noreply.github.com>
2019-07-23 18:18:26 -0400

    Uploaded files

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,31 @@
+import {songInfo} from '../../app/songInfo';
+
+export const SONG : songInfo[]=[{
+    songName:'Scared to Be lonely',
+    artist:'Martin Grrrix & Dua Lipa',
+    genre:'Future bass',
+    yearReleased:2017,
+    pic:'../assets/img/a.jpg'
+},
+{
+    songName:'Rockstar',
+    artist:'Nickelback',
+    genre:'Hard Rock',
+    yearReleased:2005,
+    pic:'../assets/img/b.jpg'
+},
+{
+    songName:'Nothing Else Matters',
+    artist:'Metallica',
+    genre:'Rock',
+    yearReleased:1991,
+    pic:'../assets/img/c.jpg'
+},
+{
+    songName:'Bring Me To Life',
+    artist:'Evanescence',
+    genre:'Nu Metal',
+    yearReleased:2003,
+    pic:'../assets/img/d.jpeg'
+},
+]
---
+++ assets/data/student.ts
@@ -0,0 +1,11 @@
+import {bhandsud} from '../../app/bhandsud';
+
+export const STUDENT: bhandsud=
+    {
+        studNo:991537942,
+        sname:'Sudarshan Bhandari',
+        loginName: 'bhandsud',
+        campus: 'Davis',
+        assignment: 'bhandsudA3'
+
+    }
